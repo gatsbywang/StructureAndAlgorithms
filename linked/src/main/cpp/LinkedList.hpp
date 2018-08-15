0c7b6ac4a907afcfb396c659fb98bdb495f81a9f
花歹 <gatsbywang@126.com>
2018-08-15 21:17:49 +0800

    add linkedList

--- linked/src/main/cpp/LinkedList.hpp
+++ linked/src/main/cpp/LinkedList.hpp
@@ -16,12 +16,14 @@ using namespace std;
 template<class E>
 struct Node {
     Node<E> *next;
+    Node<E> *prev;
     E value;
 
 public:
-    Node(E value, Node<E> *next) {
+    Node(E value, Node<E> *prev, Node<E> *next) {
         this->value = value;
         this->next = next;
+        this->prev = prev;
     }
 };
 
@@ -38,53 +40,81 @@ class LinkedList {
 
 
 public:
+    /**
+    * 添加数据
+    * @param e 元素
+    */
     void push(E e);
-
-    Node<E> *node(int index);
 
     int size();
 
     E get(int index);
 
+    // remove insert
     void insert(int index, E e);
 
-    void remove(int index);
+    E remove(int index);
 
     ~LinkedList();
+
+    Node<E> *node(int i);
+
+    void linkLast(E e);
+
+    void linkBefore(Node<E> *pNode, E e);
+
+    E unlink(Node<E> *pNode);
 };
 
 template<class E>
 void LinkedList<E>::push(E e) {
-    Node<E> *new_node = new Node<E>(e, NULL);
+    // 添加一个数据
+    linkLast(e);
+    /*Node<E> *new_node = new Node<E>(e, NULL);
 
-    if (head) {
-        //找到尾巴节点，再添加
-        /* Node<E> *h = head;
-         while (h){
-             if(h->next == NULL){
-                 break;
-             }
-             h = h->next;
-         }
-         h->next = new_node;*/
+    if (head) {// 0 -> 1 -> 2 -> 3
+        // ?
+        // head->next = new_node;
+        // 找到尾巴节点，有一个特定就是 next 节点为空
+        *//*Node<E>* h = head;
+        while(h){
+            if(h->next == NULL){
+                break;
+            }
+            h = h->next;
+        }
+        h->next = new_node;*//*
+        // 每一次都需要找到最后一个节点  50000
+        // 记录 last 节点
 
-        //记录last节点
-//        Node<E> *last = node(len - 1);
-        last->next = new_node;//O(1)
+        // Node<E> *last = node(len - 1);
+        last->next = new_node;// O(1)
     } else {
         head = new_node;
     }
-    last = new_node;
+    last = new_node;*/
+
     len++;
 }
 
 template<class E>
 Node<E> *LinkedList<E>::node(int index) {   //O(n)
-    Node<E> *h = head;
-    for (int i = 0; i < index; i++) {
-        h = h->next;
+    if(index < len >> 1){
+        // 从前往后变量
+        Node<E> *cur = head;
+        for (int i = 0; i < index; ++i) {
+            cur = cur->next;
+        }
+        return cur;
     }
-    return h;
+    else{
+        // 从后往前遍历
+        Node<E> *cur = last;
+        for (int i = len - 1; i > index; i--) {
+            cur = cur->prev;
+        }
+        return cur;
+    }
 }
 
 template<class E>
@@ -104,51 +134,122 @@ template<class E>
 void LinkedList<E>::insert(int index, E e) {
 
     assert(index >= 0 && index <= len);
+    // 考虑边界  0
+    if (index == len) {
+        linkLast(e);
+    } else {
+        linkBefore(node(index), e);
+    }
 
-    Node<E> *new_node = new Node<E>(e, NULL);
+    /*Node<E> *new_node = new Node<E>(e, NULL);
     if (index == 0) {
         Node<E> *h = head;
         head = new_node;
-
         new_node->next = h;
     } else {
-
-
-        Node<E> *new_node = new Node<E>(e, NULL);
-
+        // 考虑最后一个位置
         Node<E> *prev = node(index - 1);
-
-        Node<E> *temp = prev->next;
+        Node<E> *next = prev->next;// NULL
         prev->next = new_node;
-        new_node->next = temp;
-    }
+        new_node->next = next;
+    }*/
+
     len++;
 }
 
 template<class E>
 void LinkedList<E>::remove(int index) {
     //边界问题
+    // 考虑边界问题  0  ， len , mid
     assert(index >= 0 && index < len);
-    if (index == 0) {
-        Node<E> *h = head;
 
-        head = h->next;
-
-        delete h;
-    } else {
-        Node<E> *prev = node(index - 1);
-        Node<E> *cur = prev->next;
-
-        prev->next = cur->next;
-
-        delete cur;
-
-    }
-    len--;
+    return unlink(node(index));
+//    if (index == 0) {
+//        Node<E> *h = head;
+//
+//        head = h->next;
+//
+//        delete h;
+//    } else {
+//        Node<E> *prev = node(index - 1);
+//        Node<E> *cur = prev->next;
+//
+//        prev->next = cur->next;
+//
+//        delete cur;
+//
+//    }
+//    len--;
 }
 
 template<class E>
 LinkedList<E>::~LinkedList() {
+
+    //释放内存？析构节点指针
+    Node *h = head;
+    while(h){
+        Node<E> *next = h->next;
+        delete h;
+        h = next;
+    }
+
+    //头指针 以及尾指针 要置空
+    head = NULL;
+    last = NULL;
+
+}
+
+template<class E>
+void LinkedList<E>::linkLast(E e) {
+    Node<E> *l = last;
+    Node<E> *new_node = new Node<E>(e, l, NULL);
+    last = new_node;
+
+    if (head) {
+        l->next = new_node;
+    } else {
+        head = new_node;
+    }
+}
+
+template<class E>
+void LinkedList<E>::linkBefore(Node<E> *cur, E e) {
+    Node<E> *prev = cur->prev;// NULL
+    Node<E> *new_node = new Node<E>(e, prev, cur);
+    // 当前的上一个节点 = 新增的节点
+    cur->prev = new_node;
+    // 上一个节点的 next -> 新增的节点  , 0 特殊处理
+    if (prev) {
+        prev->next = new_node;
+    } else {// 0
+        head = new_node;
+    }
+}
+
+template<class E>
+E LinkedList<E>::unlink(Node<E> *cur) {
+    // 左右两个节点
+    Node<E> *prev = cur->prev;
+    Node<E> *next = cur->next;
+
+    E value = cur->value;
+
+    // 有两个为空的情况， 严谨 ，思维灵活
+    if(prev){
+        prev->next = next;
+    } else {
+        head = next;
+    }
+
+    if(next){
+        next->prev = prev;
+    } else {
+        last = prev;
+    }
+
+    delete cur;
+    len--;
+    return value;
 }
 
 #endif //STRUCTUREANDALGORITHMS_LINKEDLIST_HPP
